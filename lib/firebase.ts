704c838ddff2b887f852006a3d5a89fc720ae6eb
Mynul Alam <143214928+mynulyt@users.noreply.github.com>
2026-05-11 03:42:43 +0600

    Replace Firebase config with hardcoded values
    Updated firebase configuration with hardcoded values.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,13 @@
-
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-}
+  apiKey: "AIzaSyCLZbf3iLv2jqAI8wrDDLJSRe4Ef-hhUn4",
+  authDomain: "fooddeliveryapp-d2619.firebaseapp.com",
+  projectId: "fooddeliveryapp-d2619",
+  storageBucket: "fooddeliveryapp-d2619.appspot.com",
+  messagingSenderId: "595630345582",
+  appId: "1:595630345582:web:b497e2aeb4d5f4fd3ac57f",
+  measurementId: "G-KGDNQYNJLS"
+};
 
-export const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig);
